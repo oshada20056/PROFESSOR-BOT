8e5e51fc64db34c2ba9feea9235359f8ab4922fe
oshada20056 <153474645+oshada20056@users.noreply.github.com>
2024-08-08 10:00:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL',-1001825073327')
+auth_channel = environ.get('AUTH_CHANNEL','-1001825073327')
 auth_grp = environ.get('AUTH_GROUP','-1001777912716')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
@@ -57,7 +57,7 @@ SHORT_API = environ.get("SHORT_API")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL','' ))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL','-1002182452942' ))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'filmstudiodl')
 P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
